ac9496414896b2b863cddc818dae5fad9d542414
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-06-12 17:14:47 +0100

    routes up to edit done

--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -14,19 +14,45 @@ def spendings():
 #new
 #'get'
 #/spendings/new
-
+@spending_blueprint.route("/spending/new", methods=['GET'])
+def new_spending():
+    merchant = merchant_repository.select_all()
+    return render_template("/spendings/new.html", all_merchants = merchants)
 
 #create
 #'post'
 #/spendings
 
+@spending_blueprint.route("/spendings", methods=['POST'])
+def create_spending():
+    amount_spent = request.form['amount_spent']
+    tag = request.form['tag']
+    merchant = request.form['merchant']
+    merchant_id = request.form['merchant_id']
+    
+    merchant = merchant_repository.select(merchant_id)
+    spending = Spending(amount_spent, tag, merchant)
+
+    spending_repository.save(spending)
+    
 #show
 #'get'
 #/spendings/<id>
+@spending_blueprint.route("/spendings/<id>", methods=['GET'])
+def edit_spending():
+    spending = spending_repository.select(id)
+    merchant = merchant_repository.select_all()
+    return render_template("/spendings/show.html", merchant = merchant)
 
 #edit 
 #'get'
 #/spending/<id>/edit 
+
+@spending_blueprint.route("/spendings/<id>/edit", methods=['GET'])
+def edit_spendings(id):
+    spending = spending_repository.select(id)
+    merchants = merchant_repository.select_all()
+    return render_template("/spendings/edit.html", spending = spending, all_merchants = merchants )
 
 #update 
 #'post' 
